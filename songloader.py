6438a44d811f1df3079b75a1c7713fa9967fcaf2
Breadn <bylueang@gmail.com>
2022-01-08 17:22:10 -0600

    testing asyncio song loading

--- songloader.py
+++ songloader.py
@@ -1,25 +1,25 @@
 import os
 import subprocess
 
-POLL_DELAY: int = 10
 
+async def _execute_cmd(cmd):
+    subprocess.call(cmd.split())
+    return
 
-def loadsong(music_dir, filename, pre_ext, ref_ext, ytID):
+async def loadsong(music_dir, filename, pre_ext, ref_ext, ytID):
     presong_file = f"{music_dir}/{filename}{pre_ext}"
     refsong_file = f"{music_dir}/{filename}{ref_ext}"
 
-    silentsh = subprocess.Popen("/bin/bash -i".split(), 
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.DEVNULL, 
-                            stderr=subprocess.PIPE)
-
     # Song download
-    cmd = f"youtube-dl -o {presong_file} -f 140 https://www.youtube.com/watch?v={ytID}\n"
-    silentsh.stdin.write(cmd.encode())
+    cmd = f"youtube-dl -o {presong_file} -f 140 https://www.youtube.com/watch?v={ytID}"
+    print("Fetching song from YouTube...")
+    await _execute_cmd(cmd)
 
     # Song conversion
-    cmd = f"ffmpeg -i {presong_file} -c:v copy -c:a libmp3lame -q:a 4 {refsong_file}\n"
-    silentsh.stdin.write(cmd.encode())
+    cmd = f"ffmpeg -i {presong_file} -c:v copy -c:a libmp3lame -q:a 4 {refsong_file}"
+    print(f"Converting song {presong_file} to mp3...")
+    await _execute_cmd(cmd)
 
     # Presong deletion
     os.remove(presong_file)
+    print(f"{refsong_file} loaded!")
