bfffdfa6b73538ae068172c4ae4dfe3363fcb1e1
Breadn <bylueang@gmail.com>
2022-01-08 17:38:01 -0600

    testing asyncio song loading

--- raspotify.py
+++ raspotify.py
@@ -72,7 +72,7 @@ async def main():
             ytID = "cdaKIWr4wDU"
 
             if(ytID not in songset):
-                asyncio.wait(sl.loadsong(music_dir, filename, pre_ext, ref_ext, ytID))
+                await asyncio.gather(sl.loadsong(music_dir, filename, pre_ext, ref_ext, ytID))
                 songset.add(ytID)
                 print("loading song")
             else:
