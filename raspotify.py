b2011356d7460781ca1a09139d46d168b12efacc
Breadn <bylueang@gmail.com>
2022-01-07 22:24:11 -0600

    testing cmd line execution

--- raspotify.py
+++ raspotify.py
@@ -12,7 +12,7 @@ def splashscreen(ver):
 
 def exit_raspotify():
     print("asynch?")
-    cmd = "youtube-dl -o './music/%(title)s.%(ext)s' -f 140 https://www.youtube.com/watch?v=cdaKIWr4wDU"
+    cmd = "youtube-dl -o music/%(title)s.%(ext)s -f 140 https://www.youtube.com/watch?v=cdaKIWr4wDU"
     subprocess.run(cmd.split())
     print("yup")
 
