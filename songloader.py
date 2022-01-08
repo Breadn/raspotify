99b31e1e54fb0f7219acac5a22933eb8a0bfa7bd
Breadn <bylueang@gmail.com>
2022-01-08 17:55:14 -0600

    testing popen song loading

--- songloader.py
+++ songloader.py
@@ -1,11 +1,11 @@
 import os
 import subprocess
 
-silentsh = subprocess.Popen("/bin/bash -i".split(), stdin=subprocess.PIPE,
-                            stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-
 def _execute_cmd(cmd):
+    silentsh = subprocess.Popen("/bin/bash -i".split(), stdin=subprocess.PIPE,
+                            stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
     silentsh.stdin.write(cmd.split())
+    silentsh.kill()
 
 
 def loadsong(music_dir, filename, ext, ytID):
@@ -13,4 +13,4 @@ def loadsong(music_dir, filename, ext, ytID):
     cmd = f"youtube-dl -o {filepath} --extract-audio --audio-format{ext} https://www.youtube.com/watch?v={ytID}"
     _execute_cmd(cmd)
     print(f"{filepath} loaded!")
-    
+
