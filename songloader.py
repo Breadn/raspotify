f8f881d013b139924b4a0a0090c61763dbb96e28
Breadn <bylueang@gmail.com>
2022-01-08 16:44:55 -0600

    testing asyncio song loading

--- songloader.py
+++ songloader.py
@@ -4,23 +4,28 @@ import subprocess
 
 POLL_DELAY: int = 10
 
-async def loadsong(music_dir, filename, pre_ext, ref_ext, ytID):
-    song_file = f"{music_dir}/{filename}"
-
+async def _loadsong_proc(cmds, presong_file, refsong_file):
     # Song download
-    cmd = f"youtube-dl -o {music_dir}/{filename}{pre_ext} -f 140 https://www.youtube.com/watch?v={ytID}"
+    cmd = cmds[0]
     subprocess.call(cmd.split(), stdout=subprocess.DEVNULL)
-
-    while(not os.path.isfile(song_file+pre_ext)):
+    while(not os.path.isfile(presong_file)):
         asyncio.sleep(POLL_DELAY)
     
     # Song conversion
-    cmd = f"ffmpeg -i {music_dir}/{filename}{pre_ext} -c:v copy -c:a libmp3lame -q:a 4 {music_dir}/{filename}{ref_ext}"
+    cmd = cmds[1]
     subprocess.call(cmd.split(), stdout=subprocess.DEVNULL)
-
-    while(not os.path.isfile(song_file+ref_ext)):
+    while(not os.path.isfile(refsong_file)):
         asyncio.sleep(POLL_DELAY)
 
     # Presong deletion
-    cmd = f"rm -rf {music_dir}/{filename}{pre_ext}"
-    subprocess.call(cmd.split(), stdout=subprocess.DEVNULL)
+    os.remove(presong_file)
+
+async def loadsong(music_dir, filename, pre_ext, ref_ext, ytID):
+    presong_file = f"{music_dir}/{filename}{pre_ext}"
+    refsong_file = f"{music_dir}/{filename}{ref_ext}"
+    cmds = (
+        f"youtube-dl -o {presong_file} -f 140 https://www.youtube.com/watch?v={ytID}",
+        f"ffmpeg -i {presong_file} -c:v copy -c:a libmp3lame -q:a 4 {refsong_file}"
+    )
+    asyncio.run(_loadsong_proc(cmds, presong_file, refsong_file))
+
