d668e99d239234973eb5d3f6e9135c634176994a
Breadn <bylueang@gmail.com>
2022-01-08 21:29:44 -0600

    SongSearcher html scrape work

--- songsearcher.py
+++ songsearcher.py
@@ -1,7 +1,32 @@
-import urllib
+from lxml import html
+import requests
 
 class SongSearcher:
-    
+
+    MAX_RESULTS = 5
+    query_base = "https://www.youtube.com/results?search_query="
+
     def __init__(self):
-        pass
+        self.response
+        self.results
+        self.select_info = {
+            "title":None,
+            "ytID":None,
+            "duration":None,
+            "views":None
+        }
+    
+    def _update_response(self, query):
+        self.response = requests.get(f"{self.query_base}{query.replace(' ', '+')}")
+
+    def search(self, keywords):
+        self._update_response(keywords)
+        tree = html.fromstring(self.response.content)
+        self.results = tree.xpath('//*[@id="video-title"]')
+        print(self.results)
+
+
+
+
+
 
