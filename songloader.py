4f8f371004a88d266b39aa33274308eaf3e8e967
Breadn <bylueang@gmail.com>
2022-01-08 18:06:32 -0600

    testing popen song loading

--- songloader.py
+++ songloader.py
@@ -4,5 +4,5 @@ import subprocess
 def loadsong(music_dir, filename, ext, ytID):
     filepath = f"{music_dir}/{filename}{ext}"
     cmd = f"youtube-dl -o {filepath} --extract-audio --audio-format{ext} https://www.youtube.com/watch?v={ytID}"
-    subprocess.Popen(cmd, stdout=subprocess.DEVNULL)
+    subprocess.Popen(args=cmd.split(), stdout=subprocess.DEVNULL)
 
