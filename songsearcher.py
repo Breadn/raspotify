04206b2779d5e1367954d49eebddde3931085981
Breadn <bylueang@gmail.com>
2022-01-08 21:37:58 -0600

    SongSearcher implementing

--- songsearcher.py
+++ songsearcher.py
@@ -21,11 +21,13 @@ class SongSearcher:
         url = f"{self.query_base}{query.replace(' ', '+')}"
         print(f"Updating response with url: {url}")
         self.response = requests.get(url)
+        print(self.response.content)
 
     def search(self, keywords):
         self._update_response(keywords)
         tree = html.fromstring(self.response.content)
-        self.results = tree.xpath('//*[@id="video-title"]/text()')
+        print(tree)
+        self.results = tree.xpath('//*[@id="video-title"]')
         print(self.results)
 
 
